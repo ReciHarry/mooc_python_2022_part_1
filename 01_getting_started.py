3faa826b207be46d111a4b3748fbc6598ad64f8a
ReciHarry <harryhopkinscode@gmail.com>
2023-03-29 15:16:14 +0100

    Completed Part 1 - 01. Getting Started.

---
+++ 01_getting_started.py
@@ -0,0 +1,42 @@
+# This section will mostly consist of very basic print exercises.
+
+# Programming exercise: Emoticon
+# Please write a program which prints out an emoticon: :-)
+
+print(":-)")
+
+# Programming exercise: Fix the code: Seven Brothers
+# "Seitsemän veljestä" is one of the first novels ever written in Finnish. 
+# It is the story of seven orphaned brothers learning to make their way in the world (read more on Wikipedia).
+# This program is supposed to print out the names of the brothers in alphabetical order, but it's not working quite right yet. 
+# Please fix the program so that the names are printed in the correct order.
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
+
+# Programming exercise: Row, Row, Row Your Boat
+# Please write a program which prints out the following lines exactly as they are written here, punctuation and all:
+
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
+
+# Programming exercise: Minutes in a year
+# Please write a program which prints out the number of minutes in a year. 
+# Use Python code to perform the calculation, as in the previous code example.
+
+# there are 60*24 minutes in a day and 365 days in a year
+print((60 * 24) * 365)
+
+# Programming exercise: Print some code
+# Thus far, you have probably used double quotation marks " to print out strings. 
+# In addition to the double quotation marks, Python also accepts single quotation marks '.
+# This comes in handy if you ever want to print out the actual quotation marks themselves:
+
+print('print("Hello there!")')
